b740ddca1942890b7f4c7a0c746511535a69a9a2
Demian <demianadam05@gmail.com>
2023-07-05 12:45:26 -0300

    Arreglo Bootstrap, añadido codigo js y html
    Back-end:
    Añadida vista HomeClienteData en con método get_data() que devuelve un objeto JsonResponse con las URLs de inicio de sesión y crear evento.
    
    Agregada la ruta 'get-data/' que llama al método get_data() de la clase HomeClienteData y asigna el nombre 'get-data' a la ruta.
    --------------------
    Front-end:
    El archivo styles.css ahora ya no contiene bootstrap, solo el css propio. Se importo directamente desde el CDN, ademas se actualizo de la version 5.1.3 a la 5.3.0.
    --------------------
    Arreglo de codigo html:
    Se creo la etiqueta main para almacenar el contenido principal.
    
    Ahora el nav.bar esta dentro de la etiqueta header.
    --------------------
    Archivo scripts.js:
    Se agregegaron las funciones:
    
    getDjangoData(url): Realiza una solicitud HTTP a la URL especificada y devuelve los datos obtenidos en formato JSON.
    
    main_home_client(): Realiza acciones específicas en la página principal del cliente, como redirigir a las URLs de inicio de sesión y crear evento al hacer clic en los botones correspondientes.
    
    main(): Realiza una solicitud asíncrona a la ruta '/get-data/', obtiene los datos de Django y ejecuta diferentes acciones según la ruta actual del navegador.

--- apps/cliente/static/js/scripts.js
+++ apps/cliente/static/js/scripts.js
@@ -5,3 +5,46 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+async function main() {
+
+    let url = await getDjangoData('/get-data/');
+    switch (window.location.pathname) {
+        case '/':
+            await main_home_client();
+            break;
+        default:
+            console.error("Ruta no especificada");
+            break;
+    }
+}
+
+async function main_home_client() {
+    const btnIngresar = document.querySelector("#btn-ingresar");
+    const btnCreaEvento = document.querySelector("#btn-crea-evento");
+    const data = await getDjangoData('/get-data/');
+    btnIngresar.addEventListener("click", () => {
+        window.location.pathname = data.login_url;
+    });
+    btnCreaEvento.addEventListener("click", () => {
+        window.location.pathname = data.crear_evento_url;
+    });
+}
+
+async function getDjangoData(url) {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error("HTTP error " + response.status);
+    }
+    const data = await response.json();
+    return data;
+}
+
+main();
+
+
+
+
+
+
+
--- apps/cliente/views.py
+++ apps/cliente/views.py
@@ -27,7 +27,7 @@ def login_view(request):
 
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from django.http import JsonResponse
 
 # backendifts/alumno
 class ClientesView(LoginRequiredMixin, TemplateView):
@@ -45,6 +45,18 @@ class HomeClienteView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+    
+class HomeClienteData():
+    def get_data(request):
+        login_url = reverse_lazy('login')
+        crear_evento_url = reverse_lazy('crearevento')
+        data = {
+             'login_url': login_url,
+             'crear_evento_url': crear_evento_url
+        }
+        return JsonResponse(data)
+    
+
 
 
 # backendifts/alumno/<int: id>
