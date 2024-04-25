d0ef31268eb7fae217b9da532c606ca8d0993a96
Matias Munoz <matiasumz96@gmail.com>
2024-04-25 20:10:41 -0300

    Se agrega el QR (Mati)

--- apps/cliente/static/js/scripts.js
+++ apps/cliente/static/js/scripts.js
@@ -5,3 +5,20 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+function generaQR() {
+    const div = document.querySelectorAll(".ticket-container");
+console.log(div.length);
+for(let i = 0; i< div.length; i++)
+{
+    console.log(div[i].innerHTML);
+    let hash = document.querySelectorAll(".hash-container")[i];
+    //console.log(hash.innerHTML);
+    var qr = new QRCode(div[i], {
+        text: hash.innerHTML,
+       
+    });
+}
+}
+console.log("hola")
+generaQR();
